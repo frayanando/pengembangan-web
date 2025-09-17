526e9654b424a12c5110dc32187db0a70f0c1349
frayanando <frayanando@gmail.com>
2025-09-17 15:35:18 +0900

    menambah folder kondisional

---
+++ latihan-js-dasar/while.js
@@ -0,0 +1,18 @@
+//cetak bilangan genap
+
+do{
+    let i =1;
+
+    console.log ("Menampilakan bilang genap dari 1 samapi 20:");
+    while(i <= 20) {
+        if(i % 2===0){
+             console.log(i);
+        }i++
+} 
+  
+//ubah ulang  jadi false agar  tidak mengulang lagi
+ulang = false
+} while (ulang)
+    
+
+
